f55e7ebb74cbde9e271bd9e3b2b795815919b06b
SauceIsForever <164048334+SauceIsForever@users.noreply.github.com>
2025-05-29 12:58:29 -0400

    Pressing numbers updates on the display

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const grey = document.querySelectorAll(".grey");
+const number = document.getElementById("number");
 
 grey.forEach(button => {
     button.addEventListener('click', () => { 
     console.log(button.textContent);
+
+    number.textContent = (button.textContent);
+
     });
 });
 
