9069ddfecb9fc939cedd57fecf15ec6aebb1ae39
SauceIsForever <164048334+SauceIsForever@users.noreply.github.com>
2025-05-29 18:11:19 -0400

    Numbers append on display without the leading 0, Pressing the AC button clears the display

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 const grey = document.querySelectorAll(".grey");
-const number = document.getElementById("number");
+const display = document.getElementById("display");
+const clear_button = document.getElementById("clear-button");
 
 grey.forEach(button => {
     button.addEventListener('click', () => { 
-    console.log(button.textContent);
-
-    number.textContent = (button.textContent);
-
+        console.log(button.textContent);
+        appendNumber(button.textContent);
     });
 });
 
+function AC_clear() {
+    display.textContent = '0';
+}
+
+clear_button.addEventListener('click', () => {
+    AC_clear();
+});
+
+function appendNumber(p) {
+    let currentValue = display.textContent;
+    if (currentValue === '0') {
+        display.textContent = p;
+    } else {
+        display.textContent += p;
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a , b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
